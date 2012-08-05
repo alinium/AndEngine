a15e950dcaab374841ec9080a8df3a8e2f51bafe
Muhammad Ali <register2ali@gmail.com>
2012-08-05 14:01:04 -0400

    Updated Ellipse shape to draw filled shape as well as just outline

--- src/org/andengine/entity/primitive/Ellipse.java
+++ src/org/andengine/entity/primitive/Ellipse.java
@@ -60,7 +60,16 @@ public class Ellipse extends PolyLine {
 	 * Uses a default {@link HighPerformanceMeshVertexBufferObject} with the {@link VertexBufferObjectAttribute}s: {@link Rectangle#VERTEXBUFFEROBJECTATTRIBUTES_DEFAULT}.
 	 */
 	public Ellipse(final float pX, final float pY, final float pRadiusA, final float pRadiusB, final float pLineWidth, final int pResolution, final VertexBufferObjectManager pVertexBufferObjectManager, DrawMode pDrawMode, final DrawType pDrawType) {
-		super(pX, pY, buildEllipseVertices(pRadiusA, pRadiusB, pResolution), pLineWidth, pVertexBufferObjectManager, pDrawMode, pDrawType);
+		super(pX, pY, buildEllipseVertices(pRadiusA, pRadiusB, pResolution, true), pLineWidth, pVertexBufferObjectManager, pDrawMode, pDrawType);
+		
+		mResolution = pResolution;
+	}
+	
+	/**
+	 * Uses a default {@link HighPerformanceMeshVertexBufferObject} with the {@link VertexBufferObjectAttribute}s: {@link Rectangle#VERTEXBUFFEROBJECTATTRIBUTES_DEFAULT}.
+	 */
+	public Ellipse(final float pX, final float pY, final float pRadiusA, final float pRadiusB, final float pLineWidth, final int pResolution, final VertexBufferObjectManager pVertexBufferObjectManager, final DrawType pDrawType, final boolean pOutlineOnly) {
+		super(pX, pY, buildEllipseVertices(pRadiusA, pRadiusB, pResolution, pOutlineOnly), pLineWidth, pVertexBufferObjectManager, pOutlineOnly ? DrawMode.LINE_LOOP : DrawMode.TRIANGLE_FAN, pDrawType);
 		
 		mResolution = pResolution;
 	}
@@ -91,18 +100,31 @@ public class Ellipse extends PolyLine {
 	// Methods
 	// ===========================================================
 
-	private static float[] buildEllipseVertices(float pRadiusA, float pRadiusB, int pResolution) {
+	private static float[] buildEllipseVertices(float pRadiusA, float pRadiusB, int pResolution, boolean pOutlineOnly) {
 
-		float[] vertices = new float[VERTEX_SIZE * pResolution];
+		int size = VERTEX_SIZE * pResolution;
+		if (!pOutlineOnly) {
+			size += (VERTEX_SIZE * 2); // one for origin and one for last triangle
+		}
+		float[] vertices = new float[size];
 		
-		for( int i = 0; i < pResolution; i++)
+		int offset = 0;
+		if (!pOutlineOnly) {
+			vertices[Mesh.VERTEX_INDEX_X] = 0;
+			vertices[Mesh.VERTEX_INDEX_Y] = 0;
+			offset = 1;
+		}
+		
+		int end = pOutlineOnly ? pResolution : pResolution + 1;
+		
+		for( int i = 0; i < end; i++)
 		{
 			double theta = 2. * Math.PI * (double)i / (double) pResolution;
 			float x = (float) ( (double)pRadiusA * Math.cos( theta ));
 			float y = (float) ( (double)pRadiusB * Math.sin( theta ));
 			
-			vertices[(i * Mesh.VERTEX_SIZE) + Mesh.VERTEX_INDEX_X] = x;
-			vertices[(i * Mesh.VERTEX_SIZE) + Mesh.VERTEX_INDEX_Y] = y;
+			vertices[((i + offset) * Mesh.VERTEX_SIZE) + Mesh.VERTEX_INDEX_X] = x;
+			vertices[((i + offset) * Mesh.VERTEX_SIZE) + Mesh.VERTEX_INDEX_Y] = y;
 		}
 				
 		return vertices;
